d83948ca508507a61700b13a1a0422452c67043f
abdulla056 <115686255+abdulla056@users.noreply.github.com>
2025-02-16 00:01:30 +0800

    styling and comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { fetchCSV } from "./assets/data";
 
@@ -8,9 +6,9 @@ export default function App() {
   const [tableOneData, setTableOneData] = useState([]); // State for table one data
   const [tableTwoData, setTableTwoData] = useState([]); // State for table two data
 
-  useEffect(() => {
+  useEffect(() => { // Fetching data from the CSV file and setting the state
     fetchCSV().then((data) => {
-      setTableOneData(data);
+      setTableOneData(data); // setting the state for table one data
       const dataMap = Object.fromEntries(
         data.map((row) => [row.Index, row.Value]) // converting table one data into maps to easily acccess index using keys
       );
@@ -21,7 +19,7 @@ export default function App() {
         { category: "Charlie", value: (dataMap["A13"] * dataMap["A12"]) },
       ];
 
-      setTableTwoData(tableTwo);
+      setTableTwoData(tableTwo); // setting the state for table two data
     });
   }, []);
   return (
@@ -35,7 +33,7 @@ export default function App() {
           </tr>
         </thead>
         <tbody>
-          {tableOneData.map(({ Index, Value }) => (
+          {tableOneData.map(({ Index, Value }) => ( // Mapping through the table one data
             <tr key={Index}>
               <td>{Index}</td>
               <td>{Value}</td>
@@ -53,7 +51,7 @@ export default function App() {
           </tr>
         </thead>
         <tbody>
-          {tableTwoData.map(({ category, value }) => (
+          {tableTwoData.map(({ category, value }) => ( // Mapping through the table two data
             <tr key={category}>
               <td>{category}</td>
               <td>{value}</td>
--- src/assets/data.js
+++ src/assets/data.js
@@ -1,15 +1,14 @@
 async function fetchCSV() {
-    const response = await fetch('/Table_Input.csv'); 
+    const response = await fetch('/Table_Input.csv'); // Fetch the CSV file
     const data = await response.text();
 
     const rows = data.split("\n").slice(1); // Remove headers
     const parsedData = rows.map(row => {
-        const [index, value] = row.split(",");
-        return { Index: index.trim(), Value: Number(value.trim()) };
+        const [index, value] = row.split(","); // Split the row into index and value
+        return { Index: index.trim(), Value: Number(value.trim()) }; // Return the index and value as an object
     });
 
     return parsedData; 
 }
 
-export { fetchCSV };
-// fetchCSV();
+export { fetchCSV }; // export the fetch function to be used in App.jsx
