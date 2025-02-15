9780c86a4d1f7589a0ab0992fef9744aedfac623
abdulla056 <115686255+abdulla056@users.noreply.github.com>
2025-02-16 00:04:25 +0800

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ export default function App() {
   return (
     <div>
       {/* Table One */}
-      <table>
+      <table id="tableOne">
         <thead>
           <tr>
             <th>Index</th>
@@ -43,7 +43,7 @@ export default function App() {
       </table>
 
       {/* Table Two */}
-      <table>
+      <table id="tableTwo">
         <thead>
           <tr>
             <th>Category</th>
